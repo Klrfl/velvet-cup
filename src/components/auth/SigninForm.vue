66936862c4f11f3c2effd5e958d73e01d1fdfc4c
Efraim Munthe <efrayanglain@gmail.com>
2025-02-11 15:52:58 +0700

    ui: create login page

---
+++ src/components/auth/SigninForm.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+function handleSignIn(form: HTMLFormElement) {
+	const formData = new FormData(form)
+	console.log(formData)
+}
+</script>
+
+<template>
+	<form
+		class="col-start-2 col-end-[-2] md:col-start-5 md:col-span-6 grid gap-4"
+		@submit.prevent="(e) => handleSignIn(e.currentTarget as HTMLFormElement)"
+	>
+		<Label for="email">Email</Label>
+		<Input type="email" id="email" name="email" placeholder="email" required />
+
+		<Label for="password">Password</Label>
+		<Input
+			type="password"
+			id="password"
+			name="password"
+			placeholder="your secret password"
+			required
+		/>
+
+		<Button type="submit">Submit</Button>
+	</form>
+</template>
