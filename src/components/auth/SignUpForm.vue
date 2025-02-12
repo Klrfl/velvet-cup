d098234b19b0ef80bca033302dd5b06b6ad6c237
Efraim Munthe <efrayanglain@gmail.com>
2025-02-12 20:58:39 +0700

    ui: improve sign up form, center sign up and login forms

--- src/components/auth/SignUpForm.vue
+++ src/components/auth/SignUpForm.vue
@@ -1,16 +1,48 @@
 <script setup lang="ts">
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 </script>
 
 <template>
 	<form
 		@submit.prevent="(e) => console.log(e.currentTarget as HTMLFormElement)"
+		class="grid gap-4"
 	>
-		<label for="email">Email</label>
-		<Input type="text" required />
+		<Label for="email">Email</Label>
+		<Input
+			type="email"
+			name="email"
+			id="email"
+			placeholder="your email here..."
+			required
+		/>
 
-		<label for="name">Name</label>
-		<label for="password">Password</label>
-		<label for="confirm-password">Confirm Password</label>
+		<Label for="name">Name</Label>
+		<Input
+			type="text"
+			name="name"
+			id="name"
+			placeholder="your name..."
+			required
+		/>
+		<Label for="password">Password</Label>
+		<Input
+			type="password"
+			name="password"
+			id="password"
+			placeholder="your super secret password..."
+			required
+		/>
+		<Label for="confirm-password">Confirm Password</Label>
+		<Input
+			type="password"
+			placeholder="your super secret password, again"
+			name="password"
+			id="password"
+			required
+		/>
+
+		<Button type="submit">Sign up</Button>
 	</form>
 </template>
