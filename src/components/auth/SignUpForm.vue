f6537ba4cba1ac2933f82245945317a7c5af4444
Efraim Munthe <efrayanglain@gmail.com>
2025-02-12 20:52:16 +0700

    ui: put sign up and login form in one place

---
+++ src/components/auth/SignUpForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Input } from "@/components/ui/input"
+</script>
+
+<template>
+	<form
+		@submit.prevent="(e) => console.log(e.currentTarget as HTMLFormElement)"
+	>
+		<label for="email">Email</label>
+		<Input type="text" required />
+
+		<label for="name">Name</label>
+		<label for="password">Password</label>
+		<label for="confirm-password">Confirm Password</label>
+	</form>
+</template>
