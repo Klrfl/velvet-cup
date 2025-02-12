6b5a45837b3ce64fe6008a9a1966044e2e65b6c6
Efraim Munthe <efrayanglain@gmail.com>
2025-02-12 08:40:54 +0700

    ui: add primary colors to tailwind config, fix 404 layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -25,7 +25,17 @@ export default {
 				background: "hsl(var(--background))",
 				foreground: "hsl(var(--foreground))",
 				primary: {
-					DEFAULT: "hsl(var(--primary))",
+					DEFAULT: "hsl(var(--primary-400))",
+					100: "hsl(var(--primary-100))",
+					200: "hsl(var(--primary-200))",
+					300: "hsl(var(--primary-300))",
+					400: "hsl(var(--primary-400))",
+					500: "hsl(var(--primary-500))",
+					600: "hsl(var(--primary-600))",
+					700: "hsl(var(--primary-700))",
+					800: "hsl(var(--primary-800))",
+					900: "hsl(var(--primary-900))",
+					950: "hsl(var(--primary-950))",
 					foreground: "hsl(var(--primary-foreground))",
 				},
 				secondary: {
@@ -91,4 +101,3 @@ export default {
 	},
 	plugins: [animate],
 }
-
