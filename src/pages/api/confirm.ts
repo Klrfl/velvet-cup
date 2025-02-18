13297a40dda66757c8462bfc1a03774b064a0579
Efraim Munthe <efrayanglain@gmail.com>
2025-02-18 10:07:57 +0700

    feat: implement confirm order

---
+++ src/pages/api/confirm.ts
@@ -0,0 +1,61 @@
+import { db } from "@/database"
+import type { APIRoute } from "astro"
+import { z } from "astro:content"
+
+export const GET: APIRoute = async ({ request, redirect }) => {
+	const rawSearchParams = new URL(request.url).searchParams
+
+	const schema = z.object({
+		order_id: z.string().uuid(),
+		status_code: z.coerce.number(),
+		transaction_status: z.string(),
+	})
+
+	const { data: searchParams, error } = schema.safeParse(
+		Object.fromEntries(rawSearchParams.entries())
+	)
+
+	if (error || !searchParams) {
+		console.error(error)
+
+		return new Response(JSON.stringify({ message: "invalid order" }), {
+			status: 400,
+		})
+	}
+
+	const existingOrder = await db
+		.selectFrom("orders")
+		.select(["status_id"])
+		.executeTakeFirst()
+
+	if (!existingOrder) {
+		return new Response(JSON.stringify({ message: "order not found" }), {
+			status: 404,
+		})
+	}
+
+	if (existingOrder.status_id === 2) {
+		return redirect("/")
+	}
+
+	const targetOrder = await db
+		.updateTable("orders")
+		.set("status_id", 2)
+		.where("id", "=", searchParams.order_id)
+		.returningAll()
+		.executeTakeFirst()
+
+	if (!targetOrder) {
+		return new Response(
+			JSON.stringify({ message: "error when updating order status" }),
+			{ status: 404 }
+		)
+	}
+
+	return new Response(
+		JSON.stringify({
+			message: "successfully updated order status",
+			data: targetOrder,
+		})
+	)
+}
