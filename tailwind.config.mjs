f25b6045f8df38d2b1af2ac1270077d0a3b7f934
Efraim Munthe <efrayanglain@gmail.com>
2025-02-19 11:14:26 +0700

    Squash merge ui/footer into main

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -39,7 +39,17 @@ export default {
 					foreground: "hsl(var(--primary-foreground))",
 				},
 				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
+					DEFAULT: "hsl(var(--secondary-400))",
+					100: "hsl(var(--secondary-100))",
+					200: "hsl(var(--secondary-200))",
+					300: "hsl(var(--secondary-300))",
+					400: "hsl(var(--secondary-400))",
+					500: "hsl(var(--secondary-500))",
+					600: "hsl(var(--secondary-600))",
+					700: "hsl(var(--secondary-700))",
+					800: "hsl(var(--secondary-800))",
+					900: "hsl(var(--secondary-900))",
+					950: "hsl(var(--secondary-950))",
 					foreground: "hsl(var(--secondary-foreground))",
 				},
 				destructive: {
