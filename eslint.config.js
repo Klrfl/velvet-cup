8b0a672faab9a69ea89deb0a0ed69efebecb0506
Efraim Munthe <efrayanglain@gmail.com>
2025-02-23 14:46:45 +0700

    chore: setup eslint for vue

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,23 @@
-import eslintPluginAstro from "eslint-plugin-astro"
+import pluginAstro from "eslint-plugin-astro"
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
+import pluginVue from "eslint-plugin-vue"
+import vueParser from "vue-eslint-parser"
+import tsParser from "@typescript-eslint/parser"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
 	{
 		ignores: [
+			"**/*.d.ts",
 			".astro",
 			"src/database/seeds/*.ts",
 			"src/database/migrations/*.ts",
 		],
 	},
 	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	//
+
 	// Allow triple-slash references in `*.d.ts` files.
 	{
 		files: ["**/*.d.ts"],
@@ -22,11 +25,25 @@ export default tseslint.config(
 			"@typescript-eslint/triple-slash-reference": "off",
 		},
 	},
-	...eslintPluginAstro.configs.recommended,
+
+	//Astro
+	...pluginAstro.configs.recommended,
+
 	{
-		files: ["**/*.{js,ts,mjs,mts}"],
+		extends: [
+			pluginJs.configs.recommended,
+			...tseslint.configs.recommended,
+			...pluginVue.configs["flat/recommended"],
+		],
+		files: ["*.vue", "**/*.vue"],
 		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
 			globals: globals.browser,
+			parser: vueParser,
+			parserOptions: {
+				parser: tsParser,
+			},
 		},
 	}
 )
