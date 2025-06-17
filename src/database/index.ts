c8e4053de8205874247a6166869e5597e2daebb9
Efraim Munthe <efrayanglain@gmail.com>
2025-06-17 14:48:41 +0700

    refactor: change database to use url instead

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,25 @@
-import { type PoolConfig } from "pg"
 import { PostgresDialect, Kysely } from "kysely"
 import type { DB } from "./database.types"
 import pg from "pg"
 const { Pool } = pg
-
-const databaseEnvVariables = {
-	user: import.meta.env.DB_USER,
-	password: import.meta.env.DB_PASSWORD,
-	database: import.meta.env.DB_DATABASE,
-	host: import.meta.env.DB_HOST,
-	port: import.meta.env.DB_PORT,
-}
-
-for (const [key, value] of Object.entries(databaseEnvVariables)) {
-	if (!value || value === null) throw Error(`${key} expected to be not null`)
-}
+//
+// const databaseEnvVariables = {
+// 	user: import.meta.env.DB_USER,
+// 	password: import.meta.env.DB_PASSWORD,
+// 	database: import.meta.env.DB_DATABASE,
+// 	host: import.meta.env.DB_HOST,
+// 	port: import.meta.env.DB_PORT,
+// }
+//
+// for (const [key, value] of Object.entries(databaseEnvVariables)) {
+// 	if (!value || value === null) throw Error(`${key} expected to be not null`)
+// }
+//
+const databaseUrl = import.meta.env.DATABASE_URL
+if (!databaseUrl) throw Error("DATABASE_URL expected to be defined")
 
 export const dialect = new PostgresDialect({
-	pool: new Pool(databaseEnvVariables as PoolConfig),
+	pool: new Pool({ connectionString: databaseUrl }),
 })
 
 export const db = new Kysely<DB>({ dialect, log: ["error"] })
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,14 +3,7 @@
 import type { Session } from "@/lib/auth"
 
 interface ImportMetaEnv {
-	readonly DB_DATABASE: string | null
-	readonly DB_PASSWORD: string | null
-	readonly DB_USER: string | null
-	readonly DB_HOST: string | null
-	readonly DB_PORT: number | null
-	readonly DB_PORT: number | null
-	readonly MIDTRANS_CLIENT_KEY: string | null
-	readonly MIDTRANS_SERVER_KEY: string | null
+	readonly DATABASE_URL string | null
 }
 
 interface ImportMeta {
