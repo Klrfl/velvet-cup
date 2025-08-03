9bb434f4476a6febf9be9cf0131fdc8c00ad97f6
Efraim Munthe <efrayanglain@gmail.com>
2025-08-03 20:40:02 +0700

    feat: add order stats to dashboard metrics

--- src/lib/services/admin.ts
+++ src/lib/services/admin.ts
@@ -1,10 +1,18 @@
 import type { DB } from "@/database/database.types"
-import type { Kysely } from "kysely"
+import { sql, type Kysely } from "kysely"
 
 interface DashboardMetrics {
-	menu_count: number
-	order_count: number
-	customer_count: number
+	kpi: {
+		menu_count: number
+		order_count: number
+		customer_count: number
+	}
+	chart: {
+		order_stats: {
+			order_count: number
+			ordered_at: Date
+		}[]
+	}
 }
 
 interface AdminService {
@@ -22,27 +30,30 @@ export class SimpleAdminService implements AdminService {
 	 * maybe from past three days or past week
 	 * */
 	async getDashboardMetrics() {
-		const results = await this.db
-			.with("m", (qb) =>
-				qb
-					.selectFrom("menu")
-					.select(({ fn }) => fn.count<number>("menu.id").as("menu_count"))
-					.where("menu.deleted_at", "is", null)
-			)
-			.with("o", (qb) =>
-				qb
-					.selectFrom("orders")
-					.select(({ fn }) => fn.count<number>("orders.id").as("order_count"))
-			)
-			.with("c", (qb) =>
-				qb
-					.selectFrom("user")
-					.select(({ fn }) => fn.count<number>("user.id").as("customer_count"))
-					.where("user.role", "<>", "admin")
-			)
-			.selectFrom(["m", "o", "c"])
-			.select(["m.menu_count", "o.order_count", "c.customer_count"])
-			.executeTakeFirst()
+		const result = await this.db.transaction().execute(async (tx) => {
+			const kpi = await tx
+				.with("m", (qb) =>
+					qb
+						.selectFrom("menu")
+						.select(({ fn }) => fn.count<number>("menu.id").as("menu_count"))
+						.where("menu.deleted_at", "is", null)
+				)
+				.with("o", (qb) =>
+					qb
+						.selectFrom("orders")
+						.select(({ fn }) => fn.count<number>("orders.id").as("order_count"))
+				)
+				.with("c", (qb) =>
+					qb
+						.selectFrom("user")
+						.select(({ fn }) =>
+							fn.count<number>("user.id").as("customer_count")
+						)
+						.where("user.role", "<>", "admin")
+				)
+				.selectFrom(["m", "o", "c"])
+				.select(["m.menu_count", "o.order_count", "c.customer_count"])
+				.executeTakeFirst()
 
 			if (!kpi) throw new Error("failed to fetch kpi")
 
