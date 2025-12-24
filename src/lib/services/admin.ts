dfa78c0ed8ee9e9c05a588ebfb4f7b532ec5fe2f
Efraim Munthe <efrayanglain@gmail.com>
2025-12-24 15:59:35 +0700

    resolve conf

--- src/lib/services/admin.ts
+++ src/lib/services/admin.ts
@@ -44,7 +44,57 @@ export class SimpleAdminService implements AdminService {
 			.select(["m.menu_count", "o.order_count", "c.customer_count"])
 			.executeTakeFirst()
 
-		if (!results) throw new Error("error when getting dashboard count")
-		return results
+			if (!kpi) throw new Error("failed to fetch kpi")
+
+			const order_stats = await tx
+				.with(
+					"dates",
+					() =>
+						// with raw queries like this you
+						// have to write the brackets yourself
+						sql`(
+            	select generate_series(
+            		CURRENT_DATE - interval '59 days',
+            		CURRENT_DATE, '1 day'
+            	)::date as ordered_at
+            )`
+				)
+				.with("orders", (qb) =>
+					qb
+						.selectFrom("orders as o")
+						.select(() => [
+							sql<number>`coalesce(count(o.created_at)::int, 0)`.as(
+								"order_count"
+							),
+							sql<Date>`dates.ordered_at`.as("ordered_at"),
+						])
+						.groupBy(sql`o.created_at::date`)
+				)
+				.with("cancelled_orders", (qb) =>
+					qb
+						.selectFrom("orders as o")
+						.select(() => [
+							sql`coalesce(count(o.created_at)::int, 0)`.as("cancelled_count"),
+							sql<Date>``,
+						])
+						.leftJoin("order_status as os", "os.id", "o.status_id")
+						.where("os.name", "=", "cancelled")
+				)
+				.selectFrom("orders as o")
+				.select(() => [
+					sql<number>`coalesce(count(o.created_at)::int, 0)`.as("order_count"),
+					sql<Date>`dates.ordered_at`.as("ordered_at"),
+				])
+				.rightJoin("dates", (join) =>
+					join.on(sql`o.created_at::date  = dates.ordered_at`)
+				)
+				.groupBy("dates.ordered_at")
+				.orderBy("dates.ordered_at")
+				.execute()
+
+			return { kpi, chart: { order_stats } }
+		})
+
+		return result
 	}
 }
